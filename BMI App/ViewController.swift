8f36ad060d3568c73b3d3377db0403965a89d227
MaybeRT-rt <maybe.rt@icloud.com>
2023-11-08 17:58:08 +0300

    Initial Commit

---
+++ BMI App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI App
+//
+//  Created by Liz-Mary on 08.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
